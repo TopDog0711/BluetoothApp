a9948616c1ae4041cd3081918a638089db950801
mhornCricut <mhorn@cricut.com>
2018-05-15 16:49:02 -0600

    All Done

--- BluetoothApp/MainPage.xaml.cs
+++ BluetoothApp/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,76 @@ namespace BluetoothApp
         public MainPage()
         {
             this.InitializeComponent();
+
+            
+
+            BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
+            watcher.ScanningMode = BluetoothLEScanningMode.Active;
+          
+            watcher.Received += Watcher_Received; 
+            watcher.Start();
+
         }
+
+        async void UpdateUI(string message)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                OutputBox.Text = message + Environment.NewLine + OutputBox.Text;
+              
+            });
+        }
+
+        private async void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+           
+            var manufacturerSections = args.Advertisement.ManufacturerData;
+            if (manufacturerSections.Any())
+            {
+                // Only print the first one of the list
+                var manufacturerData = manufacturerSections[0];
+                var data = new byte[manufacturerData.Data.Length];
+                using (var reader = DataReader.FromBuffer(manufacturerData.Data))
+                {
+                    reader.ReadBytes(data);
+                }
+
+                UpdateUI($"MANDATA = ID: 0x{ manufacturerData.CompanyId.ToString("X")} : {BitConverter.ToString(data)}");
+               
+            }
+
+            
+
+            var dataSections = args.Advertisement.DataSections;
+            if(dataSections.Any())
+            {
+
+                foreach(var dataSection in dataSections)
+                {
+                    var data = new byte[dataSection.Data.Length];
+                    using (var reader = DataReader.FromBuffer(dataSection.Data))
+                    {
+                        reader.ReadBytes(data);
+                    }
+
+                    UpdateUI($"DATASECTION = {BitConverter.ToString(data)}");
+                  
+                }
+               
+               
+            }
+
+            BluetoothLEDevice device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+            if(device != null)
+            {
+                UpdateUI($"DEVICE = Name: { device.Name}");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
